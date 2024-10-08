2d2cf73292669a2e7c0377f1a12d8939c62d21ea
Olexiy-repin <repinlyoha@gmail.com>
2024-10-08 22:21:49 +0300

    classwork

---
+++ js/01-vars.js
@@ -0,0 +1,38 @@
+/*
+ * Змінні та типи даних
+ *
+ * - Оголошення змінних з let та const
+ * - Найменування змінних
+ * - Перевизначення значення
+ * - Вибір між const і let
+ * - Виведення даних
+ * - Отримання даних
+ * - Примітивні типи даних: number, string, boolean, null, undefined
+ * - Оператор typeof
+ */
+
+// const pricePerItem = 10;
+
+// console.log(pricePerItem);
+
+// pricePerItem = 20;
+
+// console.log(pricePerItem);
+
+// const firstName = prompt('Enter your first name');
+
+// console.log(firstName);
+
+// const isAdult = confirm('Are you adult?');
+
+// console.log(isAdult);
+
+// const a = prompt('Enter first number');
+// const b = prompt('Enter second number');
+
+// console.log(typeof a);
+// console.log(typeof b);
+
+// const total = a + b;
+
+// console.log(total);
---
+++ js/03-strings.js
@@ -0,0 +1,55 @@
+/*
+ * Рядки
+ *
+ * - Конкатенація рядків
+ * - Шаблонні рядки
+ * - Довжина рядка
+ * - Індексація елементів
+ * - Незмінність рядків
+ */
+
+//? Обʼєднайте два рядки в один і виведіть до консолі.
+//* Рішення через конкатенацію
+// const firstName = 'Chelsy';
+// const lastName = 'Emerald';
+
+// const fullName = firstName + ' ' + lastName + '.';
+
+// console.log(fullName);
+
+//* Рішення через шаблоні рядки
+// const firstName = 'Chelsy';
+// const lastName = 'Emerald';
+
+// const fullName = `${firstName} ${lastName}.`;
+
+// console.log(fullName);
+
+//? Підставте значення зміної quantity до рядка orderMsg, використоуючи шаблоні рядки.
+// const quantity = '15';
+// const orderMsg = `You order is ${quantity}`;
+
+// console.log(orderMsg);
+
+/*
+  ? Запросіть у користувача ім'я та прізвище і виведіть у консоль повідомлення:
+  ? Hello <імʼя> <прізвище>, you are logged in.
+*/
+// const firstName = prompt('Enter your first name');
+// const lastName = prompt('Enter your last name');
+
+// const message = `Hello ${firstName} ${lastName}, you are logged in.`;
+
+// console.log(message);
+
+//? Дізнайтесь довжину рядка message. Виведіть перший та останій символ рядка message.
+// let message = 'This string is 28 chars long';
+
+// const messageLength = message.length;
+// const firstCharOfMessage = message[0];
+// const lastCharOfMessage = message[message.length - 1];
+
+// console.log(message);
+// console.log(messageLength);
+// console.log(firstCharOfMessage);
+// console.log(lastCharOfMessage);
