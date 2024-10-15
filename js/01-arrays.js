9b2ceab9f51dc6b343b64b6a9775cbd44d67a73f
Olexiy-repin <repinlyoha@gmail.com>
2024-10-15 21:38:45 +0300

    classwork

---
+++ js/01-arrays.js
@@ -0,0 +1,29 @@
+/*
+ * Масиви
+ *
+ * - Створення масиву
+ * - Доступ до елементів
+ * - Перевизначення значення елемента
+ * - Довжина масиву
+ * - Індекс останнього елемента
+ */
+// ['HTML', 'CSS', 'JavaScript', 'React', 'PostgreSQL']
+
+// const courses = ['HTML', 'CSS', 'JavaScript'];
+
+// console.log(courses);
+
+// console.log(courses[0]);
+// console.log(courses[1]);
+// console.log(courses[2]);
+// console.log(courses[3]);
+
+// courses[0] = 'React';
+
+// console.log(courses);
+
+// const firstCourse = courses[0];
+// const lastCourse = courses[courses.length - 1];
+
+// console.log('firstCourse:', firstCourse);
+// console.log('lastCourse:', lastCourse);
---
+++ js/03-methods.js
@@ -0,0 +1,69 @@
+/*
+ * Методи масиву
+ *
+ * - join
+ * - split (Метод рядка)
+ * - slice
+ * - concat
+ * - indexOf
+ * - push/pop
+ */
+
+/*
+? -------------------------
+*/
+// const allUsers = ['Elva Harper', 'Lily Wilkins', 'Derek Medina', 'Charlie Boyd', 'Jon Rowe'];
+// const allUsersStr = allUsers.join(' ');
+
+// console.log('allUsers:', allUsers);
+// console.log('allUsersStr:', allUsersStr);
+
+/*
+? -------------------------
+*/
+// const phoneNumbersStr = '+380961111111,+380961111234,+380961111876';
+// const phoneNumbersArr = phoneNumbersStr.split(',');
+
+// console.log('phoneNumbersStr:', phoneNumbersStr);
+// console.log('phoneNumbersArr:', phoneNumbersArr);
+
+/*
+? -------------------------
+*/
+// const allUsers = ['Elva Harper', 'Lily Wilkins', 'Derek Medina', 'Charlie Boyd', 'Jon Rowe'];
+// const allUsersCopy = allUsers.slice(1, 3);
+
+// console.log('allUsers:', allUsers);
+// console.log('allUsersCopy:', allUsersCopy);
+
+/*
+? -------------------------
+*/
+// const oldCourses = ['HTML', 'CSS', 'JavaScript'];
+// const newCourses = ['React', 'PostgreSQL'];
+// const allCourses = oldCourses.concat(newCourses);
+
+// console.log('oldCourses: ', oldCourses);
+// console.log('newCourses: ', newCourses);
+// console.log('allCourses: ', allCourses);
+
+// allCourses[allCourses.length] = 'Angular';
+
+// console.log('allCourses: ', allCourses);
+
+// allCourses.push('Angular', 'TypeScript');
+
+// console.log('allCourses: ', allCourses);
+
+// allCourses.pop();
+// allCourses.pop();
+
+// console.log('allCourses: ', allCourses);
+
+// allCourses.unshift('TypeScript');
+
+// console.log('allCourses: ', allCourses);
+
+// allCourses.shift();
+
+// console.log('allCourses: ', allCourses);
