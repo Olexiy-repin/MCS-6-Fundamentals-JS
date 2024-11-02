a64fe5647e3992d9c7f4f3ee5a8109be0169bb34
Grygoriy Chernish <cga.jsdev@gmail.com>
2024-11-02 15:56:39 +0200

    classwork

--- js/tasks.js
+++ js/tasks.js
@@ -1,30 +1,117 @@
+// const students = [
+//   { name: 'Mango', score: 83 },
+//   { name: 'Poly', score: 59 },
+//   { name: 'Ajax', score: 37 },
+//   { name: 'Kiwi', score: 94 },
+//   { name: 'Houston', score: 64 },
+// ];
+
+// const result = students.map((student, idx, array) => {
+//   return student.score;
+// });
+// console.log(result);
+// [?, ? ,?, ?]
+
+// const values = [51, -3, 27, 21, -68, 42, -37];
+
+// const result = values.filter(() => {
+//   return 'false';
+// }); // undefined
+// console.log(result);
+
+// [?,?,?]
 /*
+
+
 ? Фільтрація за діапазоном
 ?
-? Напишіть функцію filterRange(numbers, a, b), яка приймає масив numbers, 
+? Напишіть функцію filterRange(numbers, min, max), яка приймає масив numbers, 
 ? шукає в ньому елементи більші-рівні min та менші-рівні max і віддає масив цих елементів.
 */
-// const filterRange = (numbers, min, max) => {};
+// const filterRange = (numbers, min, max) => {
+//   const filteredNumbers = numbers.filter(number => {
+//     return number >= min && number <= max;
+//   });
 
-// console.log([10, 20, 2, 3, 56, 4], 4, 56);
+//   return filteredNumbers;
+// };
+
+// const filterRange = (numbers, min, max) =>
+//   numbers.filter(number => number >= min && number <= max);
+
+// console.log(filterRange([10, 20, 2, 3, 56, 4], 4, 56));
 // console.log(filterRange([10, 20, 2, 3, 56, 4], 3, 10));
-
+// [];
 //? Відфільтруйте масив на унікальні значення.
 // const users = ['Andre', 'Lester', 'Andre', 'Lester', 'Louisa', 'Alex'];
 // const filtredUsers = [];
+
+// //Варіант 1 з forEach i includes
+// users.forEach((user, index, array) => {
+//   if (!filtredUsers.includes(user)) {
+//     filtredUsers.push(user);
+//   }
+// });
+
+// console.log(filtredUsers);
+
+//Варіант 2 з filter() та indexOf()
+// const users = ['Andre', 'Lester', 'Andre', 'Lester', 'Louisa', 'Alex'];
+// const filtredUsers = users.filter((user, index, array) => {
+//   return array.indexOf(user) === index;
+// });
+
+// ['Andre', 'Lester']
+//1 ітерація
+//0 === 0;
+//2 ітерація
+//1 === 1;
+//3 ітерація
+//0 === 2;
 
 // console.log('users:', users);
 // console.log('filtredUsers:', filtredUsers);
 
 //? Відфільтруйте масив з користувачами на унікальні значення.
-// const allUsers = [
-//   { firstName: 'Herbert', age: 25 },
-//   { firstName: 'Herbert', age: 25 },
-//   { firstName: 'Todd', age: 30 },
-//   { firstName: 'Todd', age: 30 },
-//   { firstName: 'Lida', age: 29 },
-// ];
-// const filtredUsers = [];
+const allUsers = [
+  { firstName: 'Herbert', age: 25 },
+  { firstName: 'Herbert', age: 25 },
+  { firstName: 'Todd', age: 30 },
+  { firstName: 'Todd', age: 30 },
+  { firstName: 'Lida', age: 29 },
+];
+const filtredUsers = [];
+
+//1 ітерація
+// elem = { firstName: 'Herbert', age: 25 }
+// idx = 0;
+//arr = [{ firstName: 'Herbert', age: 25 },{ firstName: 'Herbert', age: 25 },{ firstName: 'Todd', age: 30 },{ firstName: 'Todd', age: 30 },{ firstName: 'Lida', age: 29 },]
+// user = undefined
+//filtredUsers = [{ firstName: 'Herbert', age: 25 }]
+
+//2 ітерація
+// elem = { firstName: 'Herbert', age: 25 }
+// idx = 1;
+//arr = [{ firstName: 'Herbert', age: 25 },{ firstName: 'Herbert', age: 25 },{ firstName: 'Todd', age: 30 },{ firstName: 'Todd', age: 30 },{ firstName: 'Lida', age: 29 },]
+// user = { firstName: 'Herbert', age: 25 }
+//filtredUsers = [{ firstName: 'Herbert', age: 25 }]
+
+//3 ітерація
+// elem = { firstName: 'Todd', age: 30 },
+// idx = 2;
+//arr = [{ firstName: 'Herbert', age: 25 },{ firstName: 'Herbert', age: 25 },{ firstName: 'Todd', age: 30 },{ firstName: 'Todd', age: 30 },{ firstName: 'Lida', age: 29 },]
+// user = undefined
+//filtredUsers = [{ firstName: 'Herbert', age: 25 }, { firstName: 'Todd', age: 30 }]
+
+// allUsers.forEach((elem, idx, arr) => {
+//   const user = filtredUsers.find((currentUser, index, array) => {
+//     return elem.firstName === currentUser.firstName;
+//   });
+
+//   if (user === undefined) {
+//     filtredUsers.push(elem);
+//   }
+// });
 
 // console.table(allUsers);
 // console.table(filtredUsers);
@@ -82,3 +169,29 @@
 
 // console.table(makeCarsWithDiscount(allCars, 0.2));
 // console.table(makeCarsWithDiscount(allCars, 0.4));
+
+const recipeList = document.querySelector('.recipes-list');
+console.dir(recipeList);
+
+fetch('https://dummyjson.com/recipes')
+  .then(res => res.json())
+  .then(data => {
+    // console.log(data.recipes);
+    renderRecipes(data.recipes);
+  });
+
+function renderRecipes(recipes) {
+  const markup = recipes
+    .map(
+      recipe => `
+     <li class="recipes-item">
+        <img src="${recipe.image}" alt="${recipe.name}">
+        <p>${recipe.name}</p>
+        <p>Cuisine: ${recipe.cuisine}</p>
+        <p>Difficulty: ${recipe.difficulty}</p>
+      </li>
+    `
+    )
+    .join('');
+  recipeList.insertAdjacentHTML('beforeend', markup);
+}
